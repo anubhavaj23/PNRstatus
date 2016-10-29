daece1a233935ace9fb8ff97167edc74d4effe1c
anubhav <anubhavaj23@gmail.com>
2016-10-29 14:35:04 +0530

    added custom input feature

--- app/src/main/java/com/example/anubh/pnrstatus/MainActivity.java
+++ app/src/main/java/com/example/anubh/pnrstatus/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
     EditText pnr;
     Button button;
     ArrayList<HashMap<String,String>> list;
-    String address,apikey;
+    String address,apikey,url;
     TextView trainno,trainname,doj,from,to,Class,chartstatus;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,66 +52,66 @@ public class MainActivity extends AppCompatActivity {
 
       //  list = new ArrayList<HashMap<String,String>>();
 
-        address = "http://api.railwayapi.com/pnr_status/pnr/2651567991/apikey/n7guqmzi";
-        apikey  = "n7guqmzi";
+        address = "http://api.railwayapi.com/pnr_status/pnr/";
+        apikey  = "/apikey/n7guqmzi";
+
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                /*if(pnr.getText().toString().trim() == null ||  pnr.getText().toString().trim() == ""){
+                if (pnr.getText().toString().trim() == null || pnr.getText().toString().trim() == "") {
                     Toast.makeText(MainActivity.this, "Enter the PNR", Toast.LENGTH_SHORT).show();
-                }
-                else{*/
+                } else {
 
                     final RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
-                   // address += "2651567991" + "/apikey/"+apikey;
+
+                    url = address + pnr.getText().toString().trim() + apikey;
                     //JSONObject jsonObject = new JSONObject();
-                    JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(address, null,
+                    JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(url, null,
 
                             new Response.Listener<JSONObject>() {
                                 @Override
                                 public void onResponse(JSONObject response) {
-                                    try{
+                                    try {
                                         String temp;
                                         //response = response.getJSONObject("response");
                                         temp = response.get("response_code").toString();
-                                        if((temp.equals("200"))){
-                                            System.out.println("error: "+response.get("error").toString());
-                                            if(!Boolean.parseBoolean(response.get("error").toString())){
-                                                System.out.println("trainno: "+response.get("train_num").toString());
+                                        if ((temp.equals("200"))) {
+                                            System.out.println("error: " + response.get("error").toString());
+                                            if (!Boolean.parseBoolean(response.get("error").toString())) {
+                                                System.out.println("trainno: " + response.get("train_num").toString());
                                                 trainno.setText(response.get("train_num").toString());
                                                 trainname.setText(response.get("train_name").toString());
                                                 doj.setText(response.get("doj").toString());
                                                 from.setText((response.getJSONObject("boarding_point")).get("code").toString());
                                                 to.setText((response.getJSONObject("to_station")).get("code").toString());
                                                 Class.setText(response.get("class").toString());
-                                                if(response.get("chart_prepared").toString().equals("N"))
+                                                if (response.get("chart_prepared").toString().equals("N"))
                                                     chartstatus.setText("Not Prep.");
                                                 else
                                                     chartstatus.setText("Prepared");
                                             }
-                                        }
-                                        else
+                                        } else
                                             trainno.setText("error:204");
-                                    }catch (Exception e){
+                                    } catch (Exception e) {
                                         e.printStackTrace();
                                     }
                                 }
                             },
-                            new Response.ErrorListener(){
+                            new Response.ErrorListener() {
                                 @Override
                                 public void onErrorResponse(VolleyError error) {
-                             //       .setText("Something went wrong...");
+                                    //       .setText("Something went wrong...");
                                     requestQueue.stop();
                                 }
                             }
 
                     );
                     requestQueue.add(jsonObjectRequest);
-               // }
+                    // }
+                }
             }
         });
+
     }
-
 }
-
